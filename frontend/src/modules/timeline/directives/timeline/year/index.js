677dded55f0b02ade5576e39df4a12bb8268af85
Blake La Pierre <blakelapierre@gmail.com>
2016-04-28 16:39:53 -0700

    Show current month (at startup)

--- frontend/src/modules/timeline/directives/timeline/year/index.js
+++ frontend/src/modules/timeline/directives/timeline/year/index.js
@@ -11,7 +11,28 @@ module.exports = () => {
       $scope.getTimeOfDayColor = () => `rgba(255, 255, 255, 0)`;
       $scope.getInverseTimeOfDayColor = () => `rgba(255, 255, 255, 1)`;
 
-      $scope.getDayMarkerPosition = () => `${(new Date($scope.now) - new Date(new Date($scope.now).getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24) / 365 * 100}%` ; // doesn't handle leap years!
+      $scope.getDayMarkerPosition = () => `${($scope.nowDate - new Date(new Date($scope.now).getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24) / 365 * 100}%` ; // doesn't handle leap years!
+
+      $scope.currentMonth = toMonthName($scope.nowDate.getMonth());
     }]
   };
 };
+
+const months = [
+  'january',
+  'february',
+  'march',
+  'april',
+  'may',
+  'june',
+  'july',
+  'august',
+  'september',
+  'october',
+  'november',
+  'december'
+];
+
+function toMonthName(monthNumber) {
+  return months[monthNumber];
+}
