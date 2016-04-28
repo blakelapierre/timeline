f9de701ffc76a92bf71cffcffa88f62ee583bfd8
Blake La Pierre <blakelapierre@gmail.com>
2016-04-27 23:34:26 -0700

    Compute gradients with uniform stops; suncalc unused

--- frontend/src/modules/timeline/directives/timeline/time/index.js
+++ frontend/src/modules/timeline/directives/timeline/time/index.js
@@ -1,3 +1,5 @@
+import suncalc from '../../../../../util/suncalc';
+
 module.exports = () => {
   return {
     restrict: 'E',
@@ -6,17 +8,36 @@ module.exports = () => {
     template: require('./template.html'),
     link($scope, element, attributes) {
       // fails horibly if more than one day between begin and end
-      $scope.getTimeScaleBackground = () => `linear-gradient(rgba(255, 255, 255, ${getLightAlpha($scope.timeBegin)}) 0%,
-                                                             rgba(255, 255, 255, ${getLightAlpha($scope.timeCurrent)}) 50%,
-                                                             rgba(255, 255, 255, ${getLightAlpha($scope.timeEnd)}) 100%)`;
+      $scope.getTimeScaleBackground = () => `linear-gradient(${gradient(20, getLightAlpha, $scope.timeBegin, $scope.timeEnd)})`;
+      // $scope.getTimeScaleBackground = () => `linear-gradient(rgba(255, 255, 255, ${getLightAlpha($scope.timeBegin)}) 0%,
+      //                                                        rgba(255, 255, 255, ${getLightAlpha($scope.timeCurrent)}) 50%,
+      //                                                        rgba(255, 255, 255, ${getLightAlpha($scope.timeEnd)}) 100%)`;
     }
   };
 };
+
+function gradient(count, fn, start, end) {
+  const unit = 1 / (count - 1);
+
+  const components = new Array(count);
+
+  for (let i = 0; i < count; i++) components[i] = `rgba(255, 255, 255, ${fn(new Date(start + (end - start) * unit * i))}) ${unit * i * 100}%`;
+console.log(components);
+  return components.join(',');
+
+  // return (new Array(count))
+  //           .map((_, i) => `rgba(255, 255, 255, ${fn(new Date(start + (end - start) * unit * i))} ${unit * i * 100}%`)
+  //           .join(',');
+}
 
 function getPercentageOfDay(date) {
   return (((date.getHours() * 60 + date.getMinutes()) * 60) + date.getSeconds()) / (60 * 60 * 24);
 }
 
 function getLightAlpha(time) {
-  return Math.sin(-(Math.PI / 2) * (1 - 4 * getPercentageOfDay(new Date(time)))) / 2 + 0.5;
+  const times = suncalc.getTimes(new Date(time), 0, 0);
+
+  console.log(times);
+
+  return Math.sin(-(Math.PI / 2) * (1 - 4 * getPercentageOfDay(new Date(time)))) / 4 + 0.25;
 }
